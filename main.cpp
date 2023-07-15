7c5452c4b386064a80e44787e6fa8cf136397478
IoannisCh <ioannis.chantolios@ieee.org>
2023-07-15 10:44:40 +0100

    minor code corrections

--- main.cpp
+++ main.cpp
@@ -1,26 +1,26 @@
 #include "gtk/gtk.h"
-#include "webkit/webkit.h"
+#include "webkit2/webkit2.h"
 
 int main(int argc, char *argv[]) {
-    // Initialize GTKmm
-    Gtk::Main kit(argc, argv);
+    // Initialize GTK
+    gtk_init(&argc, &argv);
 
     // Create a window
-    Gtk::Window window;
-    window.set_default_size(800, 600);
+    GtkWidget* window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_default_size(GTK_WINDOW(window), 800, 600);
 
     // Create a WebView widget
     WebKitWebView* webView = WEBKIT_WEB_VIEW(webkit_web_view_new());
-    webView->load_uri("https://www.google.com");
+    webkit_web_view_load_uri(webView, "https://www.google.com");
 
     // Add the WebView to the window
-    window.add(*GTK_WIDGET(webView));
+    gtk_container_add(GTK_CONTAINER(window), GTK_WIDGET(webView));
 
-    // Show all widgets
-    window.show_all();
+    // Show the window
+    gtk_widget_show_all(window);
 
-    // Start the GTKmm main loop
-    Gtk::Main::run(window);
+    // Start the GTK main loop
+    gtk_main();
 
     return 0;
 }
