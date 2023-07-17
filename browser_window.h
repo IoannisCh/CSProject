5a8f14f2b50ddc8eadaeb3c6a860d131b0ad3e74
IoannisCh <ioannis.chantolios@ieee.org>
2023-07-17 18:40:31 +0100

    creation of Browser window

---
+++ browser_window.h
@@ -0,0 +1,22 @@
+#ifndef BROWSER_WINDOW_H
+#define BROWSER_WINDOW_H
+
+#include <gtk/gtk.h>
+#include <webkit2/webkit2.h>
+#include <vector>
+#include <navigation_bar.h>
+#include <web_view.h>
+
+class BrowserWindow{
+    public:
+        BrowserWindow();
+        ~BrowserWindow();
+        void create();
+
+    private:
+        GTkWidget* window;
+        NavigationBar navigationBar;
+        WebView* webView;
+};
+
+#endif // BROWSER_WINDOW_H
